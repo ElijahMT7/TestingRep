5329af3ae685271ff8469b5cb11cd88c41989fa9
ElijahM <ElijahM@Lab-13.kegrp.co.za>
2025-04-09 09:52:32 +0200

    First Commit

---
+++ src/gittesting/GitTesting.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gittesting;
+
+/**
+ *
+ * @author ElijahM
+ */
+public class GitTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("Hello Git");
+       System.out.print("Kganyia");
+    }
+    
+}
