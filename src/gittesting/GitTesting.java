b68dea95eb91488ec0636efd51e17cb6aa511605
ElijahM <ElijahM@Lab-13.kegrp.co.za>
2025-04-09 10:15:48 +0200

    Second Commit

--- src/gittesting/GitTesting.java
+++ src/gittesting/GitTesting.java
@@ -16,6 +16,7 @@ public class GitTesting {
     public static void main(String[] args) {
        System.out.print("Hello Git");
        System.out.print("Kganyia");
+       System.out.print("Blacksta");
     }
     
 }
